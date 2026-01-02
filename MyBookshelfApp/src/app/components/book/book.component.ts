1852176aa9ac6044b1143ea1ceb7e483be4a1564
Noahgamerrr <noah.arsic@gmx.at>
2026-01-02 14:26:34 +0100

    Styled book component

--- MyBookshelfApp/src/app/components/book/book.component.ts
+++ MyBookshelfApp/src/app/components/book/book.component.ts
@@ -1,9 +1,11 @@
 import { Component, input } from "@angular/core";
+import { BookStar } from "./bookStar/bookStar.component";
 
 @Component({
     selector: 'book',
     templateUrl: 'book.component.html',
-    styleUrl: 'book.component.scss'
+    styleUrl: 'book.component.scss',
+    imports: [ BookStar ]
 })
 export class Book {
     title = input("");
